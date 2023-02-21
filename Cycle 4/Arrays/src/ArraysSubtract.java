70d132cc35ed751ccc4db82c903bee456f463822
affffffffffffffffffffff <86025943+affffffffffffffffffffff@users.noreply.github.com>
2023-02-21 12:32:49 -0600

    update

--- Cycle 4/Arrays/src/ArraysSubtract.java
+++ Cycle 4/Arrays/src/ArraysSubtract.java
@@ -7,21 +7,21 @@ public class ArraysSubtract {
 		int[] b = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8, 7};
 		System.out.println("subtracting the arrays = " + Arrays.toString(subtract(a, b)));
 		System.out.println(Arrays.toString(a));
-		System.out.println("sum of spots 3-6 = " + sumSection(a, 3, 6));
-		System.out.println("sum of spots 2-9 = " + sumSection(a, 2, 9));
-		System.out.println("# of 4s = " + numTimes(a, 4));
-		System.out.println("# of 9s = " + numTimes(a, 9));
-		System.out.println("# of 7s = " + numTimes(a, 7));
-		System.out.println("new array with all 7s removed = " + Arrays.toString(removeNum(a, 7)));
-		System.out.println("# of 7s = " + numTimes(removeNum(a, 7), 7));
+		System.out.println("sum of spots 3-6 = " + sumBetween(a, 3, 6));
+		System.out.println("sum of spots 2-9 = " + sumBetween(a, 2, 9));
+		System.out.println("# of 4s = " + countNs(a, 4));
+		System.out.println("# of 9s = " + countNs(a, 9));
+		System.out.println("# of 7s = " + countNs(a, 7));
+		System.out.println("new array with all 7s removed = " + Arrays.toString(removeNs(a, 7)));
+		System.out.println("# of 7s = " + countNs(removeNs(a, 7), 7));
 		System.out.println(Arrays.toString(b));
-		System.out.println("sum of spots 3-16 = " + sumSection(b, 3, 16));
-		System.out.println("sum of spots 2-9 = " + sumSection(b, 2, 9));
-		System.out.println("# of 0s = " + numTimes(b, 0));
-		System.out.println("# of 3s = " + numTimes(b, 3));
-		System.out.println("# of 7s = " + numTimes(b, 7));
-		System.out.println("new array with all 7s removed = " + Arrays.toString(removeNum(b, 7)));
-		System.out.println("# of 7s = " + numTimes(removeNum(b, 7), 7));
+		System.out.println("sum of spots 3-16 = " + sumBetween(b, 3, 16));
+		System.out.println("sum of spots 2-9 = " + sumBetween(b, 2, 9));
+		System.out.println("# of 0s = " + countNs(b, 0));
+		System.out.println("# of 3s = " + countNs(b, 3));
+		System.out.println("# of 7s = " + countNs(b, 7));
+		System.out.println("new array with all 7s removed = " + Arrays.toString(removeNs(b, 7)));
+		System.out.println("# of 7s = " + countNs(removeNs(b, 7), 7));
 		
 	}// end of main
 	
@@ -33,7 +33,7 @@ public class ArraysSubtract {
 		return x;
 	}
 	
-	public static int sumSection(int[] p, int q, int r) {
+	public static int sumBetween(int[] p, int q, int r) {
 		int x = 0;
 		for (int i = q; i <= r; i++) {
 			x += p[i];
@@ -41,7 +41,7 @@ public class ArraysSubtract {
 		return x;
 	}
 	
-	public static int numTimes(int[] p, int q) {
+	public static int countNs(int[] p, int q) {
 		int x = 0;
 		for (int i = 0; i < p.length; i++) {
 			if (p[i] == q) x += 1;
@@ -49,8 +49,8 @@ public class ArraysSubtract {
 		return x;
 	}
 	
-	public static int[] removeNum(int[] p, int q) {
-		int[] x = new int[p.length - numTimes(p, q)];
+	public static int[] removeNs(int[] p, int q) {
+		int[] x = new int[p.length - countNs(p, q)];
 		int correction = 0;
 		for (int i = 0; i < p.length; i++) {
 			if (p[i] == q) {
